be9bc2e5c9f9b54e95b00f87a09bf936398889a9
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2021-12-09 21:45:45 -0400

    Added Netflix Questions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswers = ['B', 'B', 'B', 'B'];
+const correctAnswers = ['C', 'D', 'B', 'A', 'C', 'D', 'A', 'D', 'B', 'A'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
 
@@ -7,12 +7,12 @@ form.addEventListener('submit', e => {
     e.preventDefault();
     
     let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value,];
 
     // check answers 
     userAnswers.forEach((answer, index) => {
         if(answer === correctAnswers[index]){
-            score += 25;
+            score += 10;
         }
     });
 
